91ebed3132b1160ecce399df5354533a19b5a717
Charlie Bliss <charles.m.bliss@gmail.com>
2016-03-11 12:22:44 -0500

    Fix migration files.

--- db/migrate/20160311113958_create_choices.rb
+++ db/migrate/20160311113958_create_choices.rb
@@ -1,6 +1,6 @@
 class CreateChoices < ActiveRecord::Migration
   def change
-      create_table do |t|
+      create_table :choices do |t|
         t.string :content, null: false
         t.integer :question_id, null: false
         t.boolean :selected, default: false
