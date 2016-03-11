4c11d927076272fcb85169dcc53eff92dc417663
Charlie Bliss <charles.m.bliss@gmail.com>
2016-03-11 14:50:21 -0500

    complete login, logout, sign up, edit profile, profile page, and some other shit too

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,1 +1,21 @@
 #session controller
+get '/login' do
+  erb :'/users/login'
+end
+
+post '/login' do
+  @user = User.find_by(username: params[:username])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    # Right now the error messages is a string in an array
+    @errors = @user.errors.full_messages
+    erb :'/users/login'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,22 +1,8 @@
 #user controller
 
-get '/login' do
-  erb :"/users/login"
-end
-
-post '/login' do
-  @user = User.find_by(username: params[:username])
-  # if user &&
-  redirect '/users'
-end
-
-get '/logout' do
-  session.clear
-  redirect '/'
-end
-
 get '/users' do
-  @users = User.all
+  # this will eventually be removed, replaced with a redirect to homepage
+  erb :'/users/index'
 end
 
 get '/users/new' do
@@ -26,9 +12,20 @@ end
 post '/users' do
   @user = User.new(username: params[:username], password: params[:password], email: params[:email])
     if @user.save
+      session[:user_id] = @user.id
       redirect "/users/#{@user.id}"
     else
       @errors = @user.errors.full_messages
       erb :'/users/new'
     end
+end
+
+get '/users/:id' do
+  @user = User.find_by(id: params[:id])
+  erb :'/users/show'
+end
+
+get '/users/:id/edit' do
+  @user = User.find_by(id: params[:id])
+  erb :'/users/edit'
 end
