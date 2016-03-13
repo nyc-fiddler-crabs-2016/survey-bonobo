0e5cdff53a9c15efd47bf67e07d0adfe914897f2
Charlie Bliss <charles.m.bliss@gmail.com>
2016-03-13 16:53:58 -0400

    Now moves to next question when you submit an answer, redirects to home page when you finish a survey

--- app/controllers/choices.rb
+++ app/controllers/choices.rb
@@ -3,19 +3,30 @@
 #   # erb :'choices/_show', layout: false
 # end
 
-get '/surveys/:survey_id/choices/:id' do
+get '/surveys/:survey_id/questions/:question_id/choices/:id' do
+
   @survey = Survey.find_by(id: params[:survey_id])
   @choice = Choice.find_by(id: params[:id])
-  binding.pry
-  erb :'/surveys/show'
+  @question = Question.find_by(id: params[:question_id])
+  # binding.pry
+  if @question.id <= @survey.questions.length
+    erb :'/surveys/show'
+  else
+    redirect '/'
+  end
 end
 
-put '/surveys/:survey_id/choices/:id' do
-
+put '/surveys/:survey_id/questions/:question_id/choices/:id' do
+  @survey = Survey.find_by(id: params[:survey_id])
   @choice = Choice.find_by(id: params[:id])
   @choice.update(reader_id: session[:user_id], selected: params[:selected])
-
-  erb :'/surveys/_choices_update'
+  @question = Question.find_by(id: params[:question_id])
+  redirect "/surveys/#{@survey.id}/questions/#{(@question.id)}/choices/#{@choice.id}"
 end
+
+
+
+
+
 
 
